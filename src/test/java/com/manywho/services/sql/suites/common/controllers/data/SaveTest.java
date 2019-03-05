77d4fbfbfce9855df71022fbad5851ecfa5771ab
James Cullimore <james.cullimore@dell.com>
2019-03-05 14:39:34 +0000

    issue #46 added tests for lists working in MySql

--- src/test/java/com/manywho/services/sql/suites/common/controllers/data/SaveTest.java
+++ src/test/java/com/manywho/services/sql/suites/common/controllers/data/SaveTest.java
@@ -7,6 +7,8 @@ import org.junit.After;
 import org.junit.Test;
 import org.sql2o.Connection;
 
+import static junit.framework.TestCase.assertEquals;
+
 public class SaveTest extends ServiceFunctionalTest {
     @Test
     public void testCreate() throws Exception {
@@ -67,17 +69,64 @@ public class SaveTest extends ServiceFunctionalTest {
         );
     }
 
+    @Test
+    public void testUpdateMissingId () throws Exception {
+        DbConfigurationTest.setPropertiesIfNotInitialized("mysql");
+
+        try (Connection connection = getSql2o().open()) {
+            String sql = "CREATE TABLE " + escapeTableName("testtable") + " (id integer primary key auto_increment NOT NULL, data text);";
+            connection.createQuery(sql).executeUpdate();
+        }
+
+        DefaultApiRequest.saveDataRequestAndAssertion("/data",
+                "suites/common/data/save/update/update-missing-request.json",
+                configurationParameters(),
+                "suites/common/data/save/update/update-missing-response.json",
+                dispatcher
+        );
+    }
+
     private void createTestTable() throws Exception {
-        DbConfigurationTest.setPropertiesIfNotInitialized("postgresql");
         try (Connection connection = getSql2o().open()) {
             String sql = "CREATE TABLE " + escapeTableName("testtable") + " (id integer primary key auto_increment NOT NULL, data text);";
             connection.createQuery(sql).executeUpdate();
         }
     }
 
+    private void insertToTestTable(String data) throws Exception {
+        try (Connection connection = getSql2o().open()) {
+            String sql = "INSERT INTO " + escapeTableName("testtable") + " (data) VALUES ('" + data + "');";
+            connection.createQuery(sql).executeUpdate();
+        }
+    }
+
+    // thinking about taking the auto increment off the id so we manually set it
+    // and 1) know what it will be instead of relying on what number the (SQL)
+    // engine decides and 2) we then don't need to concern ourselves with how to
+    // auto increment and check the numbers in this test
+    private void insertToTestTable(int id, String data) throws Exception {
+        try (Connection connection = getSql2o().open()) {
+            String sql = "INSERT INTO " + escapeTableName("testtable") + " (id, data) VALUES ('" + id + "', '" + data + "');";
+            connection.createQuery(sql).executeUpdate();
+        }
+    }
+
+    private void assertCountOfTestTable (int expected) throws Exception {
+        try (Connection connection = getSql2o().open()) {
+            String sql = "SELECT count(*) From " + escapeTableName("testtable") + ";";
+            int found = connection.createQuery(sql).executeScalar(Integer.class);
+            assertEquals(expected, found);
+        } catch (Exception e) {
+            throw e;
+        }
+    }
+
     @Test
     public void createMulitipleTest() throws Exception {
+        DbConfigurationTest.setPropertiesIfNotInitialized("postgresql");
+
         createTestTable();
+
         DefaultApiRequest.saveDataRequestAndAssertion("/data",
                 "suites/common/data/save/create/create-multiple-request.json",
                 configurationParameters(),
@@ -88,18 +137,50 @@ public class SaveTest extends ServiceFunctionalTest {
 
     @Test
     public void updateMulitipleTest() throws Exception {
+        DbConfigurationTest.setPropertiesIfNotInitialized("postgresql");
+
         createTestTable();
 
-        try (Connection connection = getSql2o().open()) {
-            String sql = "INSERT INTO " + escapeTableName("testtable") +
-                    "(data) VALUES ('first'), ('second');";
-            connection.createQuery(sql).executeUpdate();
-        }
+        insertToTestTable("first");
+        insertToTestTable("second");
+
 
         DefaultApiRequest.saveDataRequestAndAssertion("/data",
                 "suites/common/data/save/update/update-multiple-request.json",
                 configurationParameters(),
                 "suites/common/data/save/update/update-multiple-response.json",
+                dispatcher
+        );
+    }
+
+    @Test
+    public void updateAndCreateTest() throws Exception {
+        DbConfigurationTest.setPropertiesIfNotInitialized("postgresql");
+
+        createTestTable();
+
+        insertToTestTable("first");
+
+        DefaultApiRequest.saveDataRequestAndAssertion("/data",
+                "suites/common/data/save/createAndUpdate/create-update-request.json",
+                configurationParameters(),
+                "suites/common/data/save/createAndUpdate/create-update-response.json",
+                dispatcher
+        );
+    }
+
+    @Test
+    public void updateMultipleMissingId() throws Exception {
+        DbConfigurationTest.setPropertiesIfNotInitialized("mysql");
+
+        createTestTable();
+
+        insertToTestTable("first");
+
+        DefaultApiRequest.saveDataRequestAndAssertion("/data",
+                "suites/common/data/save/update/update-multiple-missing-request.json",
+                configurationParameters(),
+                "suites/common/data/save/update/update-multiple-missing-response.json",
                 dispatcher
         );
     }
