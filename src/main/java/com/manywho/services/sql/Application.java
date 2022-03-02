3e24bc019132ac28098e01bde22a2b0e66fc08dd
tom-rooks-boomi <78496052+tom-rooks-boomi@users.noreply.github.com>
2022-03-02 16:25:51 +0000

    listener route
    change to force to listen on v1 only to test the production proxy

--- src/main/java/com/manywho/services/sql/Application.java
+++ src/main/java/com/manywho/services/sql/Application.java
@@ -23,7 +23,7 @@ public class Application extends Servlet3Server  {
 
         server.addModule(new ApplicationSqlModule());
         server.setApplication(Application.class);
-        server.start("/api/sql/2");
+        server.start("/api/sql/1");
     }
 
     private static void checkDriverClass(String driverRef) {
